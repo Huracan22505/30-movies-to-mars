336ebc303d6476c8013a1b9c94b9a09b7c378a02
Feride Shamsutdinova <ferika.sham@gmail.com>
2021-01-31 14:57:45 +0300

    feat: add js for cardPage

--- src/js/refs.js
+++ src/js/refs.js
@@ -3,4 +3,6 @@ export default {
   searchFormMobile: document.getElementById('searchFormMobile'),
   galleryBox: document.querySelector('.gallery__wrapper'),
   genreList: document.querySelector('.gallery__film-category__light'),
+  cardModal: document.querySelector('.card__modal__lightbox'),
+  cardBox: document.querySelector('.card__overlay'),
 };
