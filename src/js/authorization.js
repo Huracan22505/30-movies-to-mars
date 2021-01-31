c92fb6d15255f4d7e3e94a2845fe10d488a9037c
Marinessa393 <futurism1303@gmail.com>
2021-01-31 17:23:42 +0200

    install firebase

---
+++ src/js/authorization.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+// import 'firebase/storage';
+// import 'firebase/messaging';
+
+firebase.initializeApp({
+  apiKey: AIzaSyABHgMmII0_xvD9k6iq4L1Mf5KdyZM - ZFY,
+  authDomain: 'thirty-movies-to-mars.firebaseapp.com',
+  projectId: 'thirty-movies-to-mars',
+  storageBucket: 'thirty-movies-to-mars.appspot.com',
+  messagingSenderId: '663360674405',
+  appId: '1:663360674405:web:a99b0fcd910e46b7d7848c',
+});
