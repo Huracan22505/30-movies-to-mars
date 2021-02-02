6c05e04f4259b9e77c4185694da2302f8ba2edc7
Huracan <huracan22505@protonmail.com>
2021-02-03 01:04:00 +0200

    refactor:

--- src/library/library.js
+++ src/library/library.js
@@ -2,5 +2,5 @@ import '../sass/main.scss';
 import '../js/6changeTheme.js';
 // import '../js/AddLocalStorage.js';
 import '../js/myLibrary.js';
-import '../js/refs.js';
+// import '../js/refs.js';
 import '../js/3navigation.js';
