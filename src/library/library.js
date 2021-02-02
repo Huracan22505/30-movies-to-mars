cc6f5592ea6ebd8e351c2b9c0f4b636f926680b1
Huracan <huracan22505@protonmail.com>
2021-02-02 02:14:23 +0200

    refactor: add correct import for js library

--- src/library/library.js
+++ src/library/library.js
@@ -1,2 +1,6 @@
 import '../sass/main.scss';
-import '../js/6changeTheme';
+import '../js/6changeTheme.js';
+import '../js/AddLocalStorage.js';
+import '../js/myLibrary.js';
+import '../js/refs.js';
+// import '../js/4filmDetailsPage.js';
